8529c799980ae2350a8c6ec8194437ddb17c7d25
Marvin Ivory II <mcivory10@gmail.com>
2021-03-02 17:20:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,8 @@
-def introduction(name)
+def introduction("Josh")
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is#{name} and I am learning to program in #{language}."
-end
 
-def introduction_with_language_optional(name, language ="Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in#{language}."
-end
 
 
 
